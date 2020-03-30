3b5d3e8789479ecd0b88397c90d37fd8f3b975b4
bodhibrdr <julie.gumerman@gmail.com>
2020-03-30 13:05:05 -0600

    server routes; filename clean-up

--- index.js
+++ index.js
@@ -3,7 +3,17 @@ require('dotenv').config()
 const express = require('express');
 const server = express();
 const cors = require("cors");
-//const 
+
+const bookRoute = require('./books/book-routes.js')
+const ethosRoute = require('./books/ethos-routes.js')
+
+server.use(express.json())
+server.use(cors());
+
+//finish
+server.use("/api/books", bookRoute)
+//finish
+server.use("/api/ethos", ethosRoute)
 
 server.get('/', (req, res) => {
     res.send("the more i learn about the world, the bigger it seems and the smaller i feel..i can't decide if learning things makes me happy or if knowing things things makes me happy. either way, i would be sad if i knew everything and i would be sad if i knew nothing.")
